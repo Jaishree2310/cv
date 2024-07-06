8c537ad31271fc5a82226b79a19567ae71162734
Jaishree Singh <jaishrees23102001@gmail.com>
2024-07-07 00:13:37 +0530

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -106,7 +106,7 @@ export const RESUME_DATA = {
       title: "Fitness-Club",
       techStack: ["Typescript", "JavaScript","HTML", "CSS"],
       description:
-        "FitClub is a straightforward and visually appealing landing page specifically crafted for gyms and fitness centers. It offers a pristine and efficient online representation, aiming to allure prospective clientele and exhibit the comprehensive range of amenities and services provided by the gym.",
+        "FitClub is a straightforward and visually appealing landing page specifically crafted for gyms and fitness centers.",
       logo: JarockiMeLogo,
       link: {
         label: "netlify.com",
